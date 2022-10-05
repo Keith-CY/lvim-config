aa051a3b7ef6910ad9bfe57894527f44381ee9b6
Keith <keithwhisper@gmail.com>
2022-10-05 18:39:06 +0800

    feat: remove deprecated field

--- config.lua
+++ config.lua
@@ -40,7 +40,7 @@ lvim.builtin.telescope.defaults.path_display = {
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.setup.actions.open_file.resize_window = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
